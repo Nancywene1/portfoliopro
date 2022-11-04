d7f5b17e09de93c0cffd03ec35bab1586bed980a
Nancywene1 <115567109+Nancywene1@users.noreply.github.com>
2022-11-04 00:51:44 +0000

    Update Footer.js

--- client/src/PortfolioContainer/Home/Footer/Footer.js
+++ client/src/PortfolioContainer/Home/Footer/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     return(
         <div className='footer-container'>
             <div className='footer-parent'>
-                <img src={require ('../../../assets/Home/shape-bg')} alt='no internet connect'/>
+                <img src={require ('../../../assets/Home/shape-bg.png')} alt='no internet connect'/>
             </div>
 
         </div>
